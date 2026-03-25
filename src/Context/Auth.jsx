f4eb68252f7fe1761650a44170e8361de32c0974
nikhil-tiwari1419 <nikhiltiwari1425@gmail.com>
2026-03-25 13:14:27 +0530

    f -cmmit

--- src/Context/Auth.jsx
+++ src/Context/Auth.jsx
@@ -11,8 +11,7 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
 
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null);    const [loading, setLoading] = useState(true);
     const navigate= useNavigate();
     // loading -> pehle check karo user looged in hai ya nhai 
 
