ae16de60335372002763773d3280b1a55255ac73
nikhil-tiwari1419 <nikhiltiwari1425@gmail.com>
2026-03-26 15:58:54 +0530

    new

--- src/pages/LocalFeed.jsx
+++ src/pages/LocalFeed.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
-
+import Footer from '../Components/Footer'
 function LocalFeed() {
   return (
     <>
-    <Navbar/>
+      <Navbar />
+      <div className='min-h-screen '></div>
+      <Footer />
     </>
   )
 }
 
+
 export default LocalFeed
+
